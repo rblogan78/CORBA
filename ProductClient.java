cf864e5f954a38a299d2cc34a78ed5815f4f33ff
Rob Logan <roblogan120@gmail.com>
2016-10-17 12:21:15 +1100

    added Server and Client code

---
+++ ProductClient.java
@@ -0,0 +1,30 @@
+
+import ProductApp.*;
+import org.omg.CosNaming.*;
+import org.omg.CosNaming.NamingContextPackage.*;
+import org.omg.CORBA.*;
+
+public class ProductClient
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			ORB orb = ORB.init(args, null);
+			org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+			
+			String name = "first";
+			NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+			Product productRef = ProductHelper.narrow(ncRef.resolve_str(name));
+			
+			String description = productRef.getDescription();
+			System.out.println(description);
+
+		}
+		catch (Exception e)
+		{
+
+			e.printStackTrace();
+		}
+	}
+}
---
+++ ProductServer.java
@@ -0,0 +1,42 @@
+import ProductApp.*; 
+import org.omg.CosNaming.*; 
+import org.omg.CosNaming.NamingContextPackage.*;
+import org.omg.CORBA.*; 
+import org.omg.PortableServer.*;
+import org.omg.PortableServer.POA;
+import java.util.Properties;
+
+public class HelloServer
+{
+	public static void main(String[] args)
+	{
+    	try
+    	{
+    		ORB orb = ORB.init(args, null);
+			POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+			rootpoa.the_POAManager().activate();
+				
+     		ProductImpl productImpl = new productImpl();
+      	    productImpl.setORB(orb);
+      	
+            // get object reference from the servant
+      	    org.omg.CORBA.Object ref = rootpoa.servant_to_reference(productImpl);
+      	    Product href = productHelper.narrow(ref);
+
+            org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+        
+            String name = "first";
+            NameComponent[] path = ncRef.to_name(name);
+            ncRef.rebind(path, href);
+
+            orb.run();
+
+    	}
+		catch (Exception e)
+		{
+
+			e.printStackTrace();
+		}
+	}
+}
